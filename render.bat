cc118180a4a78a74556d13c1eac130dcb8e77d81
Enrico Atzeni <19359508+enrico-atzeni@users.noreply.github.com>
2025-01-03 09:58:34 +0100

    Update render.bat

--- render.bat
+++ render.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"C:\Program Files\Blender Foundation\Blender 4.2\blender" -d --background --python blender-script.py
+"C:\Program Files\Blender Foundation\Blender 4.2\blender" -d --background --python blender_script.py
