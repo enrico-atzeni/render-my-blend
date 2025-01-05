6e9923acd6f674c47d0f896a288e799c5ff4406b
Enrico Atzeni <atzenien@gmail.com>
2025-01-05 11:55:48 +0100

    improved camera and background position between different models with different sizes

--- blender_script.py
+++ blender_script.py
@@ -1,11 +1,25 @@
 import bpy
 import os
-import sys
-from math import cos, sin, tan
+
+pi = 22.0/7.0
+
+def eulerToDegree(euler):
+    return ( (euler) / (2 * pi) ) * 360
+
+def degreeToEuler(degree):
+    return ( (degree) / 360 ) * 2 * pi
 
 def render_360(stl_path, output_dir):
+    photo_count = 5
+    make_video = True
+    # in seconds
+    video_duration = 5
+
+    # maybe you should use an absolute path
+    background_image_path = "background.jpg"
+
     # estraggo il nome del file senza estensione dal path
-    file_name = os.path.splitext(os.path.basename(stl_path))[0]
+    stl_filename = os.path.splitext(os.path.basename(stl_path))[0]
 
     # Cancella tutti gli oggetti esistenti
     bpy.ops.wm.read_factory_settings(use_empty=True)
@@ -17,54 +31,39 @@ def render_360(stl_path, output_dir):
     # Ridimensiona la mesh importata a 10 metri di altezza
     imported_object = bpy.context.selected_objects[0]
     bpy.context.view_layer.objects.active = imported_object
-    scale_factor = 10 / imported_object.dimensions.z
+    scale_factor = 10 / max(imported_object.dimensions.x, imported_object.dimensions.y, imported_object.dimensions.z)
     imported_object.scale = (scale_factor, scale_factor, scale_factor)
     bpy.ops.object.transform_apply(scale=True)
     bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='BOUNDS')
 
+    # Move the object to have its bottom at z=0 and its center at (0, 0)
+    imported_object.location = (0,0,0)
+
     # ruoto l'oggetto sperando di metterlo frontale
-    object_base_rotation = 6.28319 / 2.75
+    object_base_rotation = 0
     imported_object.rotation_euler[2] = object_base_rotation
 
-    # recupera le coordinate boundary XYZ dell'oggetto
-    min_x = imported_object.bound_box[0][0]
-    max_x = imported_object.bound_box[0][0]
-    min_y = imported_object.bound_box[0][1]
-    max_y = imported_object.bound_box[0][1]
-    min_z = imported_object.bound_box[0][2]
-    max_z = imported_object.bound_box[0][2]
-
-    for i in range(1, 8):
-        if imported_object.bound_box[i][0] < min_x:
-            min_x = imported_object.bound_box[i][0]
-        if imported_object.bound_box[i][0] > max_x:
-            max_x = imported_object.bound_box[i][0]
-        if imported_object.bound_box[i][1] < min_y:
-            min_y = imported_object.bound_box[i][1]
-        if imported_object.bound_box[i][1] > max_y:
-            max_y = imported_object.bound_box[i][1]
-        if imported_object.bound_box[i][2] < min_z:
-            min_z = imported_object.bound_box[i][2]
-        if imported_object.bound_box[i][2] > max_z:
-            max_z = imported_object.bound_box[i][2]
-
-    x_distance = 10
-    y_distance = 10
-    z_distance = 5
+    x_distance = 14
+    y_distance = 14
+    z_distance = 7
 
     # Aggiungi una camera
-    camera_location = (max_x + x_distance, max_y + x_distance, max_z + z_distance)
-    # camera_look_at = ((min_x + max_x) / 2, (min_y + max_y) / 2, (min_z + max_z) / 2)
-    # direction = [camera_look_at[i] - camera_location[i] for i in range(3)]
+    camera_location = (x_distance, y_distance, z_distance)
     bpy.ops.object.camera_add(location=camera_location)
     camera = bpy.context.object
     camera.rotation_euler = bpy.context.scene.cursor.rotation_euler
+    # camera.rotation_euler = camera_rotation
     bpy.context.view_layer.objects.active = camera
     bpy.ops.object.constraint_add(type='TRACK_TO')
     camera.constraints["Track To"].target = imported_object
     camera.constraints["Track To"].track_axis = 'TRACK_NEGATIVE_Z'
     camera.constraints["Track To"].up_axis = 'UP_Y'
 
+    bpy.ops.object.constraint_add(type='COPY_LOCATION')
+    camera.constraints["Copy Location"].target = imported_object
+    camera.constraints["Copy Location"].use_offset = True
+
+    # add lights to the scene
     color_nerdify_pink = (1, 0.0202883, 0.346704)
     color_nerdify_green = (0.283147, 0.887924, 0.03434)
 
@@ -84,7 +83,6 @@ def render_360(stl_path, output_dir):
     area_light_2.data.energy = 800
     area_light_2.data.color = color_nerdify_green
 
-    
     # back center (from obj perspective)
     bpy.ops.object.light_add(type='AREA', align='WORLD', location=(0.9, 14, 9.7), scale=(1, 1, 1), rotation=(-3.9, -2, -3.9))
     area_light_3 = bpy.context.object
@@ -108,64 +106,80 @@ def render_360(stl_path, output_dir):
     bpy.context.scene.render.image_settings.file_format = 'PNG'
 
     # Aggiungi un'immagine di background
-    if True:
-        # maybe here you need the abs path
-        filepath = "background.jpg"
+    if background_image_path:
         # extract only file name
-        planeimagefilename = os.path.basename(filepath)
+        image_filename = os.path.basename(background_image_path)
         # extract only directory to the file
-        filepath = os.path.dirname(filepath)
+        directory_path = os.path.dirname(background_image_path)
 
-        bpy.ops.image.import_as_mesh_planes(relative=False, filepath=filepath, files=[{"name":planeimagefilename, "name":planeimagefilename}], directory=filepath)
+        bpy.ops.image.import_as_mesh_planes(relative=False, filepath=background_image_path, files=[{"name":image_filename, "name":image_filename}], directory=directory_path)
 
-        empty = bpy.context.selected_objects[0]
-        bpy.context.object.scale = (88, 88, 1)
-        bpy.context.object.rotation_euler[2] = 2.35619
-        empty.location = (-60, -60, -15)
+        bgimage = bpy.context.selected_objects[0]
+
+        bpy.ops.object.constraint_add(type='COPY_ROTATION')
+        bgimage.constraints["Copy Rotation"].target = camera
+        bgimage.constraints["Copy Rotation"].use_z = True
+        bgimage.constraints["Copy Rotation"].use_x = True
+        bgimage.constraints["Copy Rotation"].use_y = True
+
+        bpy.ops.object.constraint_add(type='COPY_LOCATION')
+        bgimage.constraints["Copy Location"].target = camera
+        bgimage.constraints["Copy Location"].use_offset = True
+
+        # because we set the constraint with an offset we set the location offset
+        bgimage.location = (-48, -47.5, -23.5)
+
+        bgimage.scale = (65, 65, 1)
         
         # add Image for background
-        bpy.ops.object.light_add(type='AREA', align='WORLD', location=(-30, -30, -2), scale=(1, 1, 1), rotation=(-0.785, 1.5, 0))
-        area_light_1 = bpy.context.object
-        area_light_1.data.energy = 9e3
+        bpy.ops.object.light_add(type='AREA', align='WORLD', location=(20, 20, 22.5), scale=(1, 1, 1), rotation=(degreeToEuler(-22), degreeToEuler(16), 0))
+        bgimage_light_1 = bpy.context.object
+        bgimage_light_1.data.energy = 9e3
+        bpy.ops.object.constraint_add(type='COPY_ROTATION')
+        bgimage_light_1.constraints["Copy Rotation"].target = bgimage
+        bgimage_light_1.constraints["Copy Rotation"].use_z = True
+        bgimage_light_1.constraints["Copy Rotation"].use_x = True
+        bgimage_light_1.constraints["Copy Rotation"].use_y = True
 
-    # capture screenshot while object rotates
-    num_screenshots = 5
-    for i in range(num_screenshots):
-        angle = object_base_rotation + (i / num_screenshots) * 6.28319  # 360 degrees in radians
+        bpy.ops.object.constraint_add(type='COPY_LOCATION')
+        bgimage_light_1.constraints["Copy Location"].target = bgimage
+        bgimage_light_1.constraints["Copy Location"].use_offset = True
+
+    # Cattura screenshot da diverse angolazioni
+    for i in range(photo_count):
+        angle = object_base_rotation + (i / photo_count) * 6.28319  # 360 degrees in radians
         imported_object.rotation_euler[2] = angle
      
-        bpy.context.scene.render.filepath = os.path.join(output_dir, f"{file_name:s}-screenshot_{i:03d}.jpg")
+        bpy.context.scene.render.filepath = os.path.join(output_dir, f"{stl_filename:s}-screenshot_{i:03d}.jpg")
         bpy.ops.render.render(write_still=True)
 
+    if make_video:
+        # Set the output format to FFMPEG and the codec to MPEG4
+        bpy.context.scene.render.image_settings.file_format = 'FFMPEG'
+        bpy.context.scene.render.ffmpeg.format = 'MPEG4'
+        bpy.context.scene.render.filepath = os.path.join(output_dir, stl_filename + ".mp4")
 
-    # Set the output format to FFMPEG and the codec to MPEG4
-    bpy.context.scene.render.image_settings.file_format = 'FFMPEG'
-    bpy.context.scene.render.ffmpeg.format = 'MPEG4'
-    bpy.context.scene.render.filepath = os.path.join(output_dir, file_name + ".mp4")
+        # duration in seconds
+        bpy.context.scene.render.fps = 25
+        frame_number = int(bpy.context.scene.render.fps * video_duration)
 
-    # duration in seconds
-    duration = 5
-    bpy.context.scene.render.fps = 25
-    frame_number = int(bpy.context.scene.render.fps * duration)
+        # Animate the rotation of the camera
+        imported_object.rotation_mode = 'XYZ'
+        imported_object.rotation_euler[2] = object_base_rotation
+        imported_object.keyframe_insert(data_path="rotation_euler", frame=i)
+        imported_object.rotation_euler[2] = object_base_rotation + 6.28319  # 360 gradi
+        imported_object.keyframe_insert(data_path="rotation_euler", frame=frame_number)
 
-    # Animate the rotation of the camera
-    imported_object.rotation_mode = 'XYZ'
-    imported_object.rotation_euler[2] = object_base_rotation
-    imported_object.keyframe_insert(data_path="rotation_euler", frame=i)
-    imported_object.rotation_euler[2] = object_base_rotation + 6.28319  # 360 gradi
-    imported_object.keyframe_insert(data_path="rotation_euler", frame=frame_number)
+        # Set the start and end frames
+        bpy.context.scene.frame_start = 1
+        bpy.context.scene.frame_end = frame_number
 
-    # Set the start and end frames
-    bpy.context.scene.frame_start = 1
-    bpy.context.scene.frame_end = frame_number
-
-    # Render the animation
-    bpy.ops.render.render(animation=True)
+        # Render the animation
+        bpy.ops.render.render(animation=True)
 
     return True
 
 if __name__ == "__main__":
-    # maybe here you need the abs path
     base_dir = ".\\"
     source_dir = os.path.join(base_dir, "source")
     completed_dir = os.path.join(base_dir, "completed")
@@ -179,16 +193,16 @@ if __name__ == "__main__":
 
     print("Starting processing")
 
-    for file_name in os.listdir(source_dir):
-        print(f"Processing {file_name}")
-        if file_name.endswith(".stl") or file_name.endswith(".obj"):
-            stl_path = os.path.join(source_dir, file_name)
+    for filename in os.listdir(source_dir):
+        print(f"Processing {filename}")
+        if filename.endswith(".stl") or filename.endswith(".obj"):
+            stl_path = os.path.join(source_dir, filename)
 
             print(f"{stl_path} - Start")
             result = render_360(stl_path, rendered_dir)
             if result:
                 print(f"{stl_path} - Done, moving to completed folder")
-                # os.rename(stl_path, os.path.join(completed_dir, file_name))
+                os.rename(stl_path, os.path.join(completed_dir, filename))
             else:
                 print(f"{stl_path} - Error")
             
